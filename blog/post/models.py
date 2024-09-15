110b2393ffde94659140b5217fe481e867ef370d
Emz <168230326+mealvarezz@users.noreply.github.com>
2024-09-15 11:32:46 -0300

    Creacion del modelo de las publicaciones

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Creación del modelo de las publicaciones.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models,models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now=True)
+    autor = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.titulo
