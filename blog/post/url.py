93d7662f05a3c8be052ba0838611f022c9d4f722
mealvarezz <mealvarez@udc.edu.ar>
2024-09-16 17:26:50 -0300

    REVISIONES

--- blog/post/url.py
+++ blog/post/url.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_publicaciones, name='lista_publicaciones'), 
-    path('create/', views.crear_publicacion, name='crear_publicacion'), 
+    path('create/', views.crear_post, name='crear_publicacion'), 
     path('<int:pk>/', views.detalles_post, name='detalles_post'),  
     path('<int:pk>/edit/', views.editar_publicacion, name='editar_publicacion'), 
     path('<int:pk>/delete/', views.eliminar_post, name='eliminar_post'), 
 ]
+
+
+#### ME EQUIVOQUE EN LOS NOMBRES REVISAR.
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .model import Post
+from .models import Post
 from .forms import PostForm
 
 # Create your views here.
